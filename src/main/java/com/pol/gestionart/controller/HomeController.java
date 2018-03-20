23782e96ca5d9b4d2be37e2b1995b1c430812cd0
Luis Méndez <luis.rains@hotmail.com>
2018-03-20 08:01:13 -0300

    agregamos templates, css y js para mostrar lindo la pagina

---
+++ src/main/java/com/pol/gestionart/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.pol.gestionart.controller;
+
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.pol.gestionart.entity.Cliente;
+import com.pol.gestionart.service.ClienteServiceImpl;
+
+@Controller
+public class HomeController {
+	
+	private static final String HOME_VIEW = "starter";
+	
+	private static final Log LOG = LogFactory.getLog(HomeController.class);
+	
+	@RequestMapping("/")
+	public ModelAndView listarClientes() {
+		LOG.info("Call: HomeController /");
+		ModelAndView mav = new ModelAndView(HOME_VIEW);
+		return mav;
+	}
+
+
+}
