67e2e50de892d9d96d49606dd5c3a2efadb83567
Skynet77 <josalvarez4@gmail.com>
2018-04-07 21:01:55 -0400

    controladores de proveedor y html's

---
+++ src/main/java/com/pol/gestionart/controller/list/ProveedorListController.java
@@ -0,0 +1,28 @@
+package com.pol.gestionart.controller.list;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pol.gestionart.dao.ProveedorDao;
+import com.pol.gestionart.dao.Dao;
+import com.pol.gestionart.entity.Proveedor;
+
+@Controller
+@Scope("session")
+@RequestMapping("proveedor")
+public class ProveedorListController extends ListController<Proveedor> {
+
+	@Autowired
+	private ProveedorDao proveedorDao;
+
+	@Override
+	public String[] getColumnas() {
+		return new String[] { "id", "nombre", "ruc", "nroTelefono", "correoElectronico", "direccion" };
+	}
+
+	@Override
+	public Dao<Proveedor> getDao() {
+		return proveedorDao;
+	}
+}
