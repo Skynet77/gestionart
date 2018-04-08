f42b876088a34087ff1c487df5477b0204f576c2
Skynet77 <josalvarez4@gmail.com>
2018-04-08 18:54:03 -0400

    vista de usuario (registrar y listar)

---
+++ src/main/java/com/pol/gestionart/controller/list/UsuarioListController.java
@@ -0,0 +1,29 @@
+package com.pol.gestionart.controller.list;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pol.gestionart.dao.Dao;
+import com.pol.gestionart.dao.UsuarioDao;
+import com.pol.gestionart.entity.Usuario;
+
+@Controller
+@Scope("session")
+@RequestMapping("usuario")
+public class UsuarioListController extends ListController<Usuario> {
+
+	@Autowired
+	private UsuarioDao usuarioDao;
+
+	@Override
+	public String[] getColumnas() {
+		return new String[] { "id", "nombreRazonSocial", "cedulaRuc", "apellido" };
+	}
+
+	@Override
+	public Dao<Usuario> getDao() {
+		return usuarioDao;
+	}
+}
