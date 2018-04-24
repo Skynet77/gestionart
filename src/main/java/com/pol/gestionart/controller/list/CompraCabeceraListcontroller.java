b340c361a6f8db0a7888ffa7119a19b28bb16c7f
Marilia Belén Sánchez Garay <mariliasanchezg93@gmail.com>
2018-04-24 15:43:55 -0400

    CompraCabeceraFormController y CompraCabeceraListController

---
+++ src/main/java/com/pol/gestionart/controller/list/CompraCabeceraListcontroller.java
@@ -0,0 +1,30 @@
+package com.pol.gestionart.controller.list;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pol.gestionart.dao.CompraCabeceraDao;
+import com.pol.gestionart.dao.Dao;
+import com.pol.gestionart.entity.CompraCabecera;
+
+@Controller
+@Scope("session")
+@RequestMapping("compra")
+public class CompraCabeceraListcontroller extends ListController<CompraCabecera> {
+	
+	@Autowired
+	private CompraCabeceraDao compraCabeceraDao;
+
+	@Override
+	public Dao<CompraCabecera> getDao() {
+		return compraCabeceraDao;
+	}
+
+	@Override
+	public String[] getColumnas() {
+		return new String[] { "id", "idUsuario", "idProveedor", "nroFactura", "fechaCompra", "monto", "tipoCompra" };
+	}
+
+}
