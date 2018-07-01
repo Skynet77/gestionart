c2c899ce6d4428a1087ed154c1ef5118f946be01
Skynet77 <josalvarez4@gmail.com>
2018-07-01 08:54:15 -0400

    probando una forma de login

--- src/main/java/com/pol/gestionart/controller/form/LoginFormController.java
+++ src/main/java/com/pol/gestionart/controller/form/LoginFormController.java
@@ -4,27 +4,42 @@ import javax.validation.Valid;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.pol.gestionart.entity.Producto;
+
+import com.pol.gestionart.bean.LoginBean;
+//import com.pol.gestionart.entity.Producto;
 
 
 
 @Controller
 @Scope("request")
 //@RequestMapping("login")
-public class LoginFormController  {
-	
-	@RequestMapping(value = "login", method = RequestMethod.GET)
-	public String accion2(ModelMap map, @Valid Producto obj,
-			BindingResult bindingResult,
-			@RequestParam(required = false) String accion,
-			@RequestParam(value = "id_objeto", required = false) Long id_objeto) {
-		
+public class LoginFormController {
+	  @RequestMapping(value = "/login", method = RequestMethod.GET)
+	  public String init(Model model) {
+	    model.addAttribute("msg", "Please Enter Your Login Details");
+	    return "login";
+	  }
+	  @RequestMapping(method = RequestMethod.POST)
+	  public String submit(Model model, @ModelAttribute("loginBean") LoginBean loginBean) {
+	    if (loginBean != null && loginBean.getUserName() != null & loginBean.getPassword() != null) {
+	      if (loginBean.getUserName().equals("chandra") && loginBean.getPassword().equals("chandra123")) {
+	        model.addAttribute("msg", loginBean.getUserName());
+	        return "success";
+	      } else {
+	        model.addAttribute("error", "Invalid Details");
+	        return "login";
+	      }
+	    } else {
+	      model.addAttribute("error", "Please enter Details");
+	      return "login";
+	    }
+	  }
 	}
-	
-}
